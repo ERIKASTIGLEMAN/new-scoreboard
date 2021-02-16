19996495e333260621349edb6e88dfaeb5db6664
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-02-16 16:19:48 -0500

    Team Input Works

--- public/main.js
+++ public/main.js
@@ -1,7 +1,29 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  const teamOneNameElement = document.querySelector('.team1 h2')
+
+  function handleTeamOneInputField(event) {
+    const teamOneTitleDisplay = event.target
+    const teamOneInputFieldValue = teamOneTitleDisplay.value
+
+    teamOneNameElement.textContent = teamOneInputFieldValue
   }
+
+  const teamOneInput = document.querySelector('.team1 input')
+
+  teamOneInput.addEventListener('input', handleTeamOneInputField)
+
+  const teamTwoNameElement = document.querySelector('.team2 h2')
+
+  function handleTeamTwoInputField(event) {
+    const teamTwoTitleDisplay = event.target
+    const teamTwoInputFieldValue = teamTwoTitleDisplay.value
+
+    teamTwoNameElement.textContent = teamTwoInputFieldValue
+  }
+
+  const teamTwoInput = document.querySelector('.team2 input')
+
+  teamTwoInput.addEventListener('input', handleTeamTwoInputField)
 }
 
 document.addEventListener('DOMContentLoaded', main)
