46bb11abf23c2b8e4db0ad765940fb58fa719f2e
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-02-17 14:08:37 -0500

    Jordan helped my reorangize code :);) & pull var from loop

--- public/main.js
+++ public/main.js
@@ -1,74 +1,51 @@
+let teamTwoScore = 0
+let teamOneScore = 0
+
+function handleTeamOneInputField(event) {
+  const teamOneTitleDisplay = event.target
+  const teamOneInputFieldValue = teamOneTitleDisplay.value
+
+  document.querySelector('.team1 h2').textContent = teamOneInputFieldValue
+}
+
+function handleTeamTwoInputField(event) {
+  const teamTwoTitleDisplay = event.target
+  const teamTwoInputFieldValue = teamTwoTitleDisplay.value
+
+  document.querySelector('.team2 h2').textContent = teamTwoInputFieldValue
+}
+
+// Player One Score
+function teamOnePlus(event) {
+  teamOneScore++
+  document.querySelector('.team1 h3').textContent = teamOneScore
+}
+
+// Player Two Score
+function teamTwoPlus(event) {
+  teamTwoScore++
+  document.querySelector('.team2 h3').textContent = teamTwoScore
+}
+
+function teamTwoSub(event) {
+  teamTwoScore--
+  document.querySelector('.team2 h3').textContent = teamTwoScore
+}
+
 function main() {
-  const teamOneNameElement = document.querySelector('.team1 h2')
-
-  function handleTeamOneInputField(event) {
-    const teamOneTitleDisplay = event.target
-    const teamOneInputFieldValue = teamOneTitleDisplay.value
-
-    teamOneNameElement.textContent = teamOneInputFieldValue
-  }
+  const teamTwoInput = document.querySelector('.team2 input')
+  teamTwoInput.addEventListener('input', handleTeamTwoInputField)
 
   const teamOneInput = document.querySelector('.team1 input')
   teamOneInput.addEventListener('input', handleTeamOneInputField)
 
-  // Player One Score
-
-  function teamOnePlus(event) {
-    let teamOneScore = 0
-    teamOneScore++
-    document.querySelector('.team1 h3').textContent = teamOneScore
-  }
-
   const teamOneScoreHeader = document.querySelector('#teamOneAdd')
   teamOneScoreHeader.addEventListener('click', teamOnePlus)
 
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
+  document.querySelector('#teamTwoAdd').addEventListener('click', teamTwoPlus)
 
-  const teamTwoNameElement = document.querySelector('.team2 h2')
-
-  function handleTeamTwoInputField(event) {
-    const teamTwoTitleDisplay = event.target
-    const teamTwoInputFieldValue = teamTwoTitleDisplay.value
-
-    teamTwoNameElement.textContent = teamTwoInputFieldValue
-  }
-
-  const teamTwoInput = document.querySelector('.team2 input')
-
-  teamTwoInput.addEventListener('input', handleTeamTwoInputField)
-
-  // Player Two Score
-
-  function teamTwoPlus(event) {
-    let teamTwoScore = 0
-    teamTwoScore++
-    document.querySelector('.team2 h3').textContent = teamTwoScore
-  }
-
-  const teamTwoScoreHeader = document.querySelector('#teamTwoAdd')
-  teamTwoScoreHeader.addEventListener('click', teamTwoPlus)
-
-  // function teamTwoSub(event) {
-  //   let teamTwoScore = 0
-  //   teamTwoScore--
-  //   document.querySelector('.team2 h3').textContent = teamTwoScore
-  // }
-
-  // const teamTwoScoreHeader = document.querySelector('#teamTwoSub')
-  // teamTwoScoreHeader.addEventListener('click', teamTwoSub)
+  const teamTwoScoreHeader = document.querySelector('#teamTwoSub')
+  teamTwoScoreHeader.addEventListener('click', teamTwoSub)
 }
 
 document.addEventListener('DOMContentLoaded', main)
