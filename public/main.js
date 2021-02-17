1c341e24370203fc5d44af6e6b6153a74f3598b1
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-02-16 23:33:58 -0500

    add index - counter++ 1 click , then no response

--- public/main.js
+++ public/main.js
@@ -9,8 +9,33 @@ function main() {
   }
 
   const teamOneInput = document.querySelector('.team1 input')
-
   teamOneInput.addEventListener('input', handleTeamOneInputField)
+
+  // Player One Score
+
+  function teamOnePlus(event) {
+    let teamOneScore = 0
+    teamOneScore++
+    document.querySelector('.team1 h3').textContent = teamOneScore
+  }
+
+  const teamOneScoreHeader = document.querySelector('#teamOneAdd')
+  teamOneScoreHeader.addEventListener('click', teamOnePlus)
+
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
 
   const teamTwoNameElement = document.querySelector('.team2 h2')
 
@@ -24,6 +49,26 @@ function main() {
   const teamTwoInput = document.querySelector('.team2 input')
 
   teamTwoInput.addEventListener('input', handleTeamTwoInputField)
+
+  // Player Two Score
+
+  function teamTwoPlus(event) {
+    let teamTwoScore = 0
+    teamTwoScore++
+    document.querySelector('.team2 h3').textContent = teamTwoScore
+  }
+
+  const teamTwoScoreHeader = document.querySelector('#teamTwoAdd')
+  teamTwoScoreHeader.addEventListener('click', teamTwoPlus)
+
+  // function teamTwoSub(event) {
+  //   let teamTwoScore = 0
+  //   teamTwoScore--
+  //   document.querySelector('.team2 h3').textContent = teamTwoScore
+  // }
+
+  // const teamTwoScoreHeader = document.querySelector('#teamTwoSub')
+  // teamTwoScoreHeader.addEventListener('click', teamTwoSub)
 }
 
 document.addEventListener('DOMContentLoaded', main)
